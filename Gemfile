4f92831ae951451ea84cf09635dcd0ac781900c9
yisaketsega <yisaketsega@gmail.com>
2018-09-07 13:41:30 -0400

    payment provided

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
 gem 'jquery-rails'
-
+gem 'stripe'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+Rails.configuration.stripe = {
+     :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIP_TEST_SECRET_KEY']
+  } 
+  
+  
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
